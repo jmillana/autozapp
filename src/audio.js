2e05d98a35f9b061abd4ee1fabad81424604b2d4
jmillana <jordi@marimon.tech>
2023-10-02 20:57:07 +0200

    ✨ feat(audio): Add function to convert ogg to wav
    As ogg is not accepted as input format by the openai/whisper API, this commit adds a new function `oggToWav` to the `audio.js` file.

--- src/audio.js
+++ src/audio.js
@@ -6,6 +6,35 @@
  *
  * */
 const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
+asyncExec = util.promisify(exec);
+
+/**
+ * Convert the given audio file in .ogg format into
+ * a file with the same name in .wav format
+ *
+ * @param {string} audioFile - ogg file to be converted
+ *
+ * */
+async function oggToWav(audioFile) {
+	inputPath = path.parse(audioFile);
+	if (inputPath.ext !== '.ogg') {
+		console.log('Not an ogg file');
+		return null;
+	}
+	console.log();
+	wavFile = `${inputPath.dir}/${inputPath.name}.wav`;
+
+	const command = `ffmpeg -y -i ${audioFile} -vn ${wavFile}`;
+	try {
+		await asyncExec(command);
+		return wavFile;
+	} catch (error) {
+		console.log('Error while converting the audio file:', error);
+		throw error;
+	}
+}
 
 /*
 /**
